c2bb0347784e064476777138b2b4b00249bef896
pbattin <prestonbattin@Prestons-MacBook-Pro.local>
2017-02-22 16:49:48 -0500

    Added toArray method and test

--- src/main/java/MySet.java
+++ src/main/java/MySet.java
@@ -1,5 +1,60 @@
 /**
  * Created by prestonbattin on 2/22/17.
  */
-public class MySet {
+public class MySet<T> {
+
+    private  T[] set;
+
+
+    MySet(){
+
+        set = (T[]) new Object[0];
+    }
+
+    public boolean add(T t){
+
+        if(!contains(t)){
+
+            T[] temp = (T[]) new Object[size()+1];
+            for(int i = 0; i < size(); i++){
+
+                temp[i] = set[i];
+            }
+
+            temp[size()] = t;
+            set = temp;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean contains(Object o){
+
+        for(T temp: set){
+
+            if(temp.equals(o))
+                return true;
+        }
+        return false;
+    }
+
+    public int size(){
+
+        return set.length;
+    }
+
+    public void clear(){
+
+        set =  (T[]) new Object[0];
+    }
+
+    public <T> T[] toArray(){
+
+
+        return (T[]) set;
+
+
+    }
+
+
 }
--- src/test/java/MySetTest.java
+++ src/test/java/MySetTest.java
@@ -1,5 +1,39 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by prestonbattin on 2/22/17.
  */
 public class MySetTest {
+
+    MySet<Integer> set;
+
+
+
+    @Before
+    public void setUp(){
+
+        set = new MySet<Integer>();
+        set.add(1);
+        set.add(2);
+    }
+
+    @Test
+    public void addTest(){
+
+        set.add(1);
+        int expected = 2;
+        int actual = set.size();
+        Assert.assertEquals("Tesint it does not add if element is present already", expected, actual);
+    }
+
+    @Test
+    public void toArrayTest(){
+
+        int expected = 1;
+       Object[] data = set.toArray();
+       int actual = (Integer) data[0];
+        Assert.assertEquals("Testing toArray", expected, actual);
+    }
 }
